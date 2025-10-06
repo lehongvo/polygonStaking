71e6cd737572da93607abcee2251f43d56c5c94d
vincentVo <lehongvi19x@gmail.com>
2025-10-06 20:38:24 +0700

    [VOLH][Admin][contract] update bytecode deploy contract

--- contracts/ChallengeDetail/ChallengeDetailV2.sol
+++ contracts/ChallengeDetail/ChallengeDetailV2.sol
@@ -1108,6 +1108,8 @@ contract ChallengeDetailV2 is IERC721Receiver {
      * @dev Send daily results to update contract activities.
      * @param _day An array of uint256 values representing days.
      * @param _stepIndex An array of uint256 values representing step indices.
+     * @param _data A tuple of two uint64 values.
+     * @param _signature The signature to be validated.
      * @param _listGachaAddress An array of addresses representing Gacha contract addresses.
      * @param _listNFTAddress An array of addresses representing NFT contract addresses.
      * @param _listIndexNFT An array of arrays representing NFT indices.
@@ -1123,8 +1125,8 @@ contract ChallengeDetailV2 is IERC721Receiver {
     function sendDailyResult(
         uint256[] memory _day,
         uint256[] memory _stepIndex,
-        // uint64[2] memory _data,
-        // bytes calldata _signature,
+        uint64[2] memory _data,
+        bytes calldata _signature,
         address[] memory _listGachaAddress,
         address[] memory _listNFTAddress,
         uint256[][] memory _listIndexNFT,
@@ -1132,12 +1134,12 @@ contract ChallengeDetailV2 is IERC721Receiver {
         bool[] memory _statusTypeNft,
         uint64[2] memory _timeRange
     ) public available onTimeSendResult onlyChallenger {
-        // IExerciseSupplementNFT(erc721Address[0]).checkValidSignature(
-        //     _day,
-        //     _stepIndex,
-        //     _data,
-        //     _signature
-        // );
+        IExerciseSupplementNFT(erc721Address[0]).checkValidSignature(
+            _day,
+            _stepIndex,
+            _data,
+            _signature
+        );
 
         uint dayLength = _day.length;
         bool isSendSameDay;
